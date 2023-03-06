ee77a8048c52ac0aae9cb030c974fec61693b31d
Ryan Williams <ryan.williams926@icloud.com>
2023-03-06 15:39:31 -0500

    basic envent handlers

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,3 +7,17 @@ var multiplayerPeer = ENetMultiplayerPeer.new()
 # port number isn't too important unless reserved (past first 1024 is safe)
 const PORT = 9999
 const ADDRESS = "localhost"
+
+
+func _on_buttonhost_pressed():
+	get_node("container-network-info/network-role").text = "Server"
+	get_node("container-menu").visible = false
+
+
+func _on_buttonjoin_pressed():
+	get_node("container-network-info/network-role").text = "Client"
+	get_node("container-menu").visible = false
+
+
+func _on_messageinput_text_submitted(new_text):
+	pass # Replace with function body.
