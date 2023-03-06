92b0c56ab16e540376580d7aef38fdd69fb85fcf
Ryan Williams <ryan.williams926@icloud.com>
2023-03-06 16:39:28 -0500

    client and host each have a player but they're not together in scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -20,6 +20,17 @@ func _on_buttonhost_pressed():
 	multiplayer.multiplayer_peer = multiplayerPeer
 	# each peer on network has unique id, display this machine's unique id
 	get_node("container-network-info/label-peer-id").text = str(multiplayer.get_unique_id())
+	
+	# instantiate character for host to control
+	add_player_character()
+	
+	# connect a lambda function to multiplayerPeer, called when a peer connects to this host.
+	multiplayerPeer.peer_connected.connect(
+		# anonymous function always given the id of the new peer machine
+		func (new_peer_id):
+			await get_tree().create_timer(1).timeout
+			rpc("add_newly_connected_player_character")
+	)
 
 func _on_buttonjoin_pressed():
 	# hide buttons, show this machine is a client
@@ -31,9 +42,16 @@ func _on_buttonjoin_pressed():
 	
 	multiplayer.multiplayer_peer = multiplayerPeer
 	get_node("container-network-info/label-peer-id").text = str(multiplayer.get_unique_id())
-	
-	
 
+# when called, instantiate the player scene, add instance to scene
+func add_player_character():
+	var playerCharacter = preload("res://scenes/player.tscn").instantiate()
+	add_child(playerCharacter)
+
+# remotely-callable function 
+@rpc
+func add_newly_connected_player_character():
+	add_player_character()
 
 func _on_messageinput_text_submitted(new_text):
 	pass # Replace with function body.
