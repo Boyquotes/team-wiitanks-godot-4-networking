2e0e72130e0b04eca8f50ec478e91a1463d17869
Ryan Williams <ryan.williams926@icloud.com>
2023-03-27 15:13:12 -0400

    added sinusoidal motion to ball object

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,8 +3,7 @@ extends Node2D
 # an object that handles various networking tasks
 var multiplayerPeer = ENetMultiplayerPeer.new()
 
-# info on what computer to connect to. for development, just loopback to self.
-# port number isn't too important unless reserved (past first 1024 is safe)
+# info on what computer to connect to. for development, just loopback to self.# port number isn't too important unless reserved (past first 1024 is safe)
 const PORT = 9999
 const TEST_ADDRESS = "127.0.0.1"
 const TESTING_LOCAL = false
