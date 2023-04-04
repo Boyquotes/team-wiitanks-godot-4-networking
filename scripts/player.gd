bfea5194ae2c2448565c6a9f2f8f8323b9926455
Ryan Williams <ryan.williams926@icloud.com>
2023-04-03 21:55:33 -0400

    updated player.gd style to match spec in GH-6

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,21 +3,23 @@ extends CharacterBody2D
 # movement speed of the player
 const SPEED = 300.0
 
+
 func _ready():
 	name = str(get_multiplayer_authority())
 	get_node("label-name").text = str(name)
 	
 	# initialize self in a random position
-	var random = RandomNumberGenerator.new()
+	var random := RandomNumberGenerator.new()
 	random.randomize()
 	position = Vector2(get_viewport().size.x * random.randf(), get_viewport().size.y * random.randf())
+
 
 # update input and movement every physics tick
 func _physics_process(_delta):
 	
 	# allow movement if this node's unique id matches that of the multiplayer authority
-	if(is_multiplayer_authority()):
-		var velocityVec = Vector2()
+	if is_multiplayer_authority():
+		var velocityVec := Vector2()
 
 		# add horizontal motion to vector.
 		if Input.is_action_pressed("ui_right"):
@@ -41,6 +43,8 @@ func _physics_process(_delta):
 		
 		rpc("remote_set_position", global_position)
 
+
 @rpc("unreliable")
-func remote_set_position(authority_position):
+func remote_set_position(authority_position) -> void:
 	global_position = authority_position
+
